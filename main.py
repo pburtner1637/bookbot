e7ed249cb90581f025926bad40c18c5faf2df77a
pburtner1637 <pburtner9312@gmail.com>
2025-03-13 21:43:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
+def count_words(text):
+    """Counts the number of words in a string."""
+    words = text.split()
+    return len(words)
+
 def main():
-    """Reads and prints the contents of the frankenstein.txt file."""
+    """Reads the book, counts words, and prints the result."""
     book_text = get_book_text("./books/frankenstein.txt")
-    print(book_text)
+    num_words = count_words(book_text)
+    print(f"{num_words} words found in the document")
 
 if __name__ == "__main__":
     main()
