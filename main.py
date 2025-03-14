8ae1340e8829383074ad0ed13b5eb2d2dbced8b9
pburtner1637 <pburtner9312@gmail.com>
2025-03-13 21:34:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_text(filepath):
+    """Reads a file and returns its contents as a string."""
+    with open(filepath) as f:
+        return f.read()
+
+def main():
+    """Reads and prints the contents of the frankenstein.txt file."""
+    book_text = get_book_text("./books/frankenstein.txt")
+    print(book_text)
+
+if __name__ == "__main__":
+    main()
