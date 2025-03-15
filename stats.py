0bbf26d3a55b7cff422172459f91f5d7876c8b2b
pburtner1637 <pburtner9312@gmail.com>
2025-03-14 22:57:17 -0500

    Update stats.py
    added count_characters

--- stats.py
+++ stats.py
@@ -2,3 +2,13 @@ def count_words(text):
     """Counts the number of words in a string."""
     words = text.split()
     return len(words)
+
+def count_characters(text):
+    """Counts the occurrences of each character in a string."""
+    char_counts = {}
+    for char in text.lower():
+        if char in char_counts:
+            char_counts[char] += 1
+        else:
+            char_counts[char] = 1
+    return char_counts
