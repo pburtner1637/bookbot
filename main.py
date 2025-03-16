46ea041920b5dc3473033e83f266d168cf31bd48
pburtner1637 <pburtner9312@gmail.com>
2025-03-15 21:39:11 -0500

    Update main.py
    added report logic

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, sort_char_counts
 
 def get_book_text(filepath):
     """Reads a file and returns its contents as a string."""
@@ -10,9 +10,17 @@ def main():
     book_text = get_book_text("./books/frankenstein.txt")
     num_words = count_words(book_text)
     char_counts = count_characters(book_text)
+    sorted_char_counts = sort_char_counts(char_counts)
 
-    print(f"{num_words} words found in the document")
-    print(char_counts)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in sorted_char_counts:
+        if item["char"].isalpha():
+            print(f"{item['char']}: {item['count']}")
+    print("============= END ===============")
 
 if __name__ == "__main__":
     main()
